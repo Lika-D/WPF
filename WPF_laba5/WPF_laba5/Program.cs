66b44e2ced0c2aaa5734706991027c61a9be6061
Lika-D <55454606+Lika-D@users.noreply.github.com>
2020-01-06 00:58:01 +0300

    Add files via upload

---
+++ WPF_laba5/WPF_laba5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+// Either change the following namespace to the name of your project,   
+// or name your project with the following name when you create it.  
+namespace WPF_laba5
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
